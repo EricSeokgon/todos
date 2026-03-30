379f056d558ceb5a909eb8c910ae12e5dbf9ac5b
hadesyi <leesk55@gmail.com>
2026-03-30 15:05:48 +0900

    feat: add PATCH endpoint for updating existing todos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 from response import TodoResponse
-from schema.request import TodoCreateRequest
+from schema.request import TodoCreateRequest, TodoUpdateRequest
 
 app = FastAPI()
 
@@ -20,3 +20,15 @@ def create_todo_handler(body: TodoCreateRequest):
     }
     todos_db.append(new_todo)
     return new_todo
+
+@app.patch("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+def update_todo_handler(todo_id: int, body: TodoUpdateRequest):
+    for todo in todos_db:
+        if todo["id"] == todo_id:
+            if body.title is not None:
+                todo["title"] = body.title
+            if body.is_done is not None:
+                todo["is_done"] = body.is_done
+            return todo
+    
+    raise HTTPException(status_code=404, detail="Todo not found")
