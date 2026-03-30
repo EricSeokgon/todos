25233694877adc0e1e6b05e87a32133b316f0ca0
hadesyi <leesk55@gmail.com>
2026-03-30 14:40:07 +0900

    feat: add POST /todos endpoint with TodoCreateRequest schema validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, status
 from response import TodoResponse
+from schema.request import TodoCreateRequest
 
 app = FastAPI()
 
@@ -9,3 +10,13 @@ todos_db = []
 @app.get("/todos", response_model=list[TodoResponse], status_code=status.HTTP_200_OK)
 def get_todos_handler():
     return todos_db
+
+@app.post("/todos", status_code=status.HTTP_201_CREATED)
+def create_todo_handler(body: TodoCreateRequest):
+    new_todo = {
+        "id": len(todos_db) + 1,
+        "title": body.title,
+        "is_done": body.is_done
+    }
+    todos_db.append(new_todo)
+    return new_todo
