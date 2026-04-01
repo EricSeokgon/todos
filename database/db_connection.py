53f73bb9e9d27b7a91be17dcf7a22365d9c06626
Eric <leesk55@gmail.com>
2026-04-01 09:57:53 +0900

    Update database connection string to remove password
    Remove password from database connection string.

--- database/db_connection.py
+++ database/db_connection.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 
 import urllib.parse
 
-password = urllib.parse.quote_plus("Dkdldkf!@#$")
-DATABASE_URL = f"mysql+pymysql://root:{password}@192.168.188.41:3306/fastapi_db"
+password = urllib.parse.quote_plus("")
+DATABASE_URL = f"mysql+pymysql://root:{password}@:3306/fastapi_db"
 
 engine = create_engine(DATABASE_URL)
 SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
