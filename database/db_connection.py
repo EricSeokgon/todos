72fe9715ef1c8878be5c726a06fde2eec2c2ee4f
hadesyi <leesk55@gmail.com>
2026-04-01 09:54:01 +0900

    feat: initialize SQLAlchemy database connection and session factory for MySQL

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://fastapi:fastapi1!@192.168.188.41:3306/fastapi_db"
+import urllib.parse
+
+password = urllib.parse.quote_plus("Dkdldkf!@#$")
+DATABASE_URL = f"mysql+pymysql://root:{password}@192.168.188.41:3306/fastapi_db"
 
 engine = create_engine(DATABASE_URL)
 SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
